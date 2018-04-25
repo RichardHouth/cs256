9f5c8dea3639a0d3f780364f90c3ae6a7f2ed820
Richard Houth <rhouth2301@yahoo.com>
2018-04-24 20:50:57 -0700

    Add files via upload

---
+++ quiz2/quiz22.cpp
@@ -0,0 +1,68 @@
+/*
+ * quiz22.cpp
+ *
+ *  Created on: Apr 24, 2018
+ *      Author: richardhouth
+ */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+//Richard Houth
+//CS 256 QUIZ 2
+
+class Circle {
+private:
+    double radius;
+    double pi = 3.14159;
+
+public:
+
+    Circle(){
+        radius = 0.0;
+    }
+
+    Circle(double r){
+        radius = r;
+    }
+
+    void setRadius(double r){
+        radius = r;
+    }
+
+
+    double getRadius(){
+        return radius;
+    }
+
+    double getArea(){
+
+        return pi * getRadius() * getRadius();
+    }
+
+    double getDiameter(){
+        return 2*getRadius();
+    }
+
+    double getCircumference(){
+        return 2 * pi * getRadius();
+    }
+
+};
+
+int main(int argc, char *argv[]) {
+
+    double user;
+    cout << "Please enter the radius: " << endl;
+    cin >> user;
+
+    Circle circle;
+    circle.setRadius(user);
+    cout << "Circumference is: " << circle.getCircumference() << endl;
+    cout << "Area is : " << circle.getArea() << endl;
+    cout << "diameter is : " << circle.getDiameter() << endl;
+
+
+    return 0;
+}
