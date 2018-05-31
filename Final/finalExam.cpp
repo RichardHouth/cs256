ae5b0fb9e311607a5a19da8c90ea617ee486acaa
Richard Houth <rhouth2301@yahoo.com>
2018-05-31 13:08:43 -0700

    Add files via upload

---
+++ Final/finalExam.cpp
@@ -0,0 +1,41 @@
+//Richard Houth
+//CS 256
+#include "finalExam.h";
+#include <string>
+using namespace std;
+
+//default
+finalExam::finalExam() {
+
+}
+
+finalExam operator +(finalExam &k) {
+	finalExam output;
+	return output;
+}
+
+finalExam operator -(finalExam &k) {
+	finalExam output;
+	return output;
+}
+
+finalExam operator *(finalExam &k) {
+	finalExam output;
+	return output;
+}
+
+finalExam operator %(finalExam &k) {
+	finalExam output;
+	return output;
+}
+
+finalExam operator /(finalExam &k) {
+	finalExam output;
+	return output;
+}
+
+finalExam::~finalExam() {
+
+}
+
+return 0;
---
+++ Final/finalExam.h
@@ -0,0 +1,25 @@
+//Richard Houth
+//CS 256
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+class finalExam {
+private:
+
+	vector <char> num;
+public:
+
+	finalExam();
+	finalExam(string n);
+	string ToString(finalExam other);
+	finalExam operator +(const finalExam &k) const;
+	finalExam operator -(const finalExam &k) const;
+	finalExam operator *(const finalExam &k) const;
+	finalExam operator /(const finalExam &k) const;
+	finalExam operator %(const finalExam &k) const;
+	~finalExam();
+};
